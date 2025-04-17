62037abd7a8eac0794daf2727d2c5f3f9158cf67
Turkish <turkishawesome@gmail.com>
2025-04-17 20:14:31 +0300

    Додав веб сторінку для перегляду фактів

--- src/main/java/com/turkish/dailyfact/controller/FactController.java
+++ src/main/java/com/turkish/dailyfact/controller/FactController.java
@@ -3,28 +3,35 @@ package com.turkish.dailyfact.controller;
 import com.turkish.dailyfact.model.Fact;
 import com.turkish.dailyfact.service.FactService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
-@RequestMapping("/api/fact")
+import java.time.LocalDate;
+
+@Controller
+@RequestMapping("/")
 @RequiredArgsConstructor
 public class FactController {
 
     private final FactService factService;
+    private final LocalDate date = LocalDate.now();
+
+    @GetMapping("/")
+    public String showFactOfTheDay(Model model) {
+        Fact fact = factService.getDailyFact();
+        model.addAttribute("fact", fact);
+        model.addAttribute("date", date);
+        return "index"; // шаблон fact-of-the-day.html
+    }
 
     @GetMapping("/random")
-    public Fact getRandomFact() {
-        return factService.getRandomFact();
-    }
-
-    @GetMapping("/today")
-    public Fact getDailyFact() {
-        return factService.getDailyFact();
-    }
-
-    @PostMapping("/add")
-    public void addFact(@RequestBody Fact fact) {
-        factService.setFact(fact);
+    public String showRandomFact(Model model) {
+        Fact fact = factService.getRandomFact();
+        model.addAttribute("fact", fact);
+        model.addAttribute("date", date);
+        return "index";
     }
 
 }
